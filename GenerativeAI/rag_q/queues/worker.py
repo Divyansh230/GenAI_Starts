4a83f845bfe6a954f3a7aef122eac292098aa654
Divyansh230 <divyanshsinghh2304@gmail.com>
2025-11-15 13:44:44 +0530

    Ascynchronous operation with python and rag operation

---
+++ GenerativeAI/rag_q/queues/worker.py
@@ -0,0 +1,69 @@
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+# Embeddings (Gemini)
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001"
+)
+
+# Connect to Qdrant
+vector_db = QdrantVectorStore.from_existing_collection(
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="Learning_Rag"
+)
+
+# Gemini 2.5 Flash model
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+def process_query(user_query:str):
+
+    print(f"Serching chunks : {user_query}")
+    search_results = vector_db.similarity_search(query=user_query, k=4)
+
+    # Prepare context
+    context = "\n\n".join([
+        f"Page Content: {result.page_content}\n"
+        f"Page Number: {result.metadata.get('page_label')}\n"
+        f"File Location: {result.metadata.get('source')}"
+        for result in search_results
+    ])
+
+        # System prompt
+    SYSTEM_PROMPT = """
+    You are a helpful assistant who answers the user query ONLY using the given context.
+    Do NOT hallucinate.  
+    If the answer is not in the context, say:
+    "I couldn't find the answer in the provided document."
+
+    Also guide the user to the correct page number.
+
+    Context:
+    {context}
+    """
+
+        # Build prompt template
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", SYSTEM_PROMPT),
+        ("human", "{query}")
+    ])
+
+    chain=prompt|model
+
+    # Get final output
+    response = chain.invoke({
+        "context": context,
+        "query": user_query
+    })
+    print(f"🤖:{reponse.content}")
+    return response.content
---
+++ GenerativeAI/rag_q/server.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI,Query
+from .client.rq_client import queue
+from .queues.worker import process_query
+
+load_dotenv()
+app=FastAPI()
+
+@app.get('/')
+def root():
+    return{'status':'Server is up and Running'}
+
+
+@app.post('/chat')
+def chat(
+    query:str=Query(...,discription="The chat query of the user")
+):
+    job=queue.enqueue(process_query,query)
+    return {"status":"queued","job_id":job.id}
+
+@app.get('/job_status')
+def get_result(
+        job_id:str=Query(...,discription="Job id")
+):
+    job=queue.fetch_job(job_id=job_id)
+    result=job.return_value()
+
+    return {"result":result}
+
